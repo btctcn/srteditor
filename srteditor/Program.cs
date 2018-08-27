a60729dab835a09dde8fbd9dd5c26e96df6ea083
abutcitcyn <abutcitcyn@os33.com>
2018-08-27 15:14:02 +0300

    initial commit

---
+++ srteditor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace srteditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("/users/ab/Downloads/subs.srt");
+            var lines = text.Split("\n\n");
+            var sb = new StringBuilder();
+            int sub = 1;
+            foreach(var line in lines)
+            {
+                if (line.Contains("[") || line.Contains("]")) continue;
+                var subLines = line.Split("\n");
+                var newLine = sub++.ToString();
+                for (int i = 1; i < subLines.Length; i++)
+                {
+                    newLine += "\n" + subLines[i];
+                }
+                newLine += "\n";
+                sb.AppendLine(newLine);
+            }
+            File.WriteAllText("/users/ab/downloads/subsnew.srt", sb.ToString());
+        }
+    }
+}
